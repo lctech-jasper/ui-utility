705d4537f04b7ded721c9aed94d9562063791f05
jasper <jasper@lctech.com.tw>
2021-05-24 15:32:43 +0800

    update

--- packages/wrap/src/index.ts
+++ packages/wrap/src/index.ts
@@ -1,7 +1,7 @@
 import {LitElement, css, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
-@customElement('wrap-ele')
+@customElement('lit-wrap')
 export class Wrap extends LitElement {
   // Define scoped styles right with your component, in plain CSS
   static styles = css`
@@ -10,10 +10,21 @@ export class Wrap extends LitElement {
     }
   `;
 
+  @property()
+  username = 'i am q'
+
+  @property({ type: Object })
+  usernameInfo = {
+    address: '605 Maybelle Fords'
+  }
+
+
   render() {
     return html`
       <div>
         <div class="wrap_header">awrap web componen</div>
+        <div class="wrap_header">only string: ${this.username}</div>
+        <div class="wrap_header">only object: ${this.usernameInfo.address}</div>
         <slot></slot>
       </div>
     `;
